18732c82544e93656c1e2121cb80ffa02ca023c2
Paco Le berre <pacocmoi@live.fr>
2023-03-18 10:19:29 +0100

    Best ft_filter you could ever dream of, np

--- Python_0_starting/ex06/filterstring.py
+++ Python_0_starting/ex06/filterstring.py
@@ -1,16 +1,20 @@
 import sys as sus
 
 
+from ft_filter import ft_filter
+
+
 def ft_filterstring(words, nb):
     """Takes a string S and an integer N as argument and print
         a list of words in S that contains more than N characters."""
     try:
         realnumber = int(nb)
     except ValueError:
-        return print("AssertionError: argument is not an integer")
+        assert False, "AssertionError: argument is not an integer"
     words_list = words.split(" ")
-    words_list = list(filter(lambda word: len(word) > realnumber, words_list))
-    print([word for word in words_list if len(word) > realnumber])
+    words_list = list(
+        ft_filter(lambda word: len(word) > realnumber, words_list))
+    print(words_list)
 
 
 if __name__ == "__main__":
--- Python_0_starting/ex06/ft_filter.py
+++ Python_0_starting/ex06/ft_filter.py
@@ -1,9 +1,14 @@
-import sys as sus
-
-
 def ft_filter(function, iterable):
     '''filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.'''
-    
+    if function is None:
+        return iter([word for word in iterable if bool(word) is True])
+    return iter([word for word in iterable if bool(function(word)) is True])
+
+
+# a = [0, 1, 2, False, True, "", "chjew"]
+
+# print(list(ft_filter(None, a)))
+# print(list(filter(None, a)))
