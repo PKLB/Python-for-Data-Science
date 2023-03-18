5997a070aa299beb40575e20b085ffbb441f11a9
Paco Le berre <pacocmoi@live.fr>
2023-03-18 09:34:41 +0100

    ex05 done for GOOD

--- Python_0_starting/ex05/building.py
+++ Python_0_starting/ex05/building.py
@@ -1,4 +1,4 @@
-import sys as sus
+import sys as su
 
 
 def main(stringo):
@@ -7,6 +7,9 @@ def main(stringo):
     punctuation characters and spaces and outputsthe sums of each."""
     [uppercase_counter := 0, lowercase_counter := 0, punctuation_counter := 0,
         space_counter := 0, digit_counter := 0]
+    punctuation_chars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+',
+                         ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',
+                         '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
     for i in stringo:
         if (i.isupper()):
             uppercase_counter = uppercase_counter + 1
@@ -16,7 +19,7 @@ def main(stringo):
             digit_counter = digit_counter + 1
         elif (i.isspace() or i == '\n' or i == '\t' or i == '\r'):
             space_counter = space_counter + 1
-        else:
+        elif i in punctuation_chars:
             punctuation_counter = punctuation_counter + 1
     print("The text contains", len(stringo), "characters:")
     print(uppercase_counter, "upper letters")
@@ -26,11 +29,17 @@ def main(stringo):
     print(digit_counter, "digits")
 
 
+# if no args -> prompt readline
+# if one arg -> goes to main
+# if more than one arg -> assertionerror
 if __name__ == "__main__":
-    if len(sus.argv) == 2:
-        main(sus.argv[1])
-    if len(sus.argv) > 2:
-        print("AssertionError: more than one argument are provided")
-    elif len(sus.argv) != 2:
-        print("What is the text to count?")
-        main(sus.stdin.readline())
+    try:
+        assert len(
+            su.argv) < 3, "AssertionError: more than one argument are provided"
+        if len(su.argv) < 2:
+            print("What is the text to count?")
+            main(su.stdin.readline())
+        else:
+            main(su.argv[1])
+    except (KeyboardInterrupt, AssertionError) as bad_args:
+        print(bad_args)
