1f9d250726460b7428ce69298cb4fddaddd100e1
Paco Le berre <pacocmoi@live.fr>
2023-03-19 15:30:42 +0100

    ex02 done and normed

---
+++ Python_1_Array/ex02/load_image.py
@@ -0,0 +1,21 @@
+from PIL import Image, UnidentifiedImageError
+import numpy as nompy
+
+
+def ft_load(path: str) -> nompy.array:
+    type = [".jpeg", ".jpg"]
+    try:
+        assert isinstance(path, str), "wrong typeee 🤣🤣🤣"
+        assert Image.open(path).format not in type, "wrong format 🤣🤣🤣"
+    except (UnidentifiedImageError, AssertionError) as bad_args:
+        print(bad_args)
+        return
+    except FileNotFoundError:
+        print("File not found:", path)
+        return
+    img = Image.open(path)
+    img.show(img)
+    print("The shape of image is:", nompy.array(img).shape)
+    print(nompy.array(img))
+    img.load()
+    return nompy.array(img)
