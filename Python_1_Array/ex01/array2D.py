4414b559b54dc0c5dd9f7c5fa671584d6ee4baf0
Paco Le berre <pacocmoi@live.fr>
2023-03-19 10:51:34 +0100

    Ex01 seems ok, everything is normed

---
+++ Python_1_Array/ex01/array2D.py
@@ -0,0 +1,19 @@
+import numpy as nompy
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """takes as parameters a 2D array that print its shape, and returns
+    a truncated version with start and end arguments."""
+    try:
+        assert all(isinstance(element, (int, float))
+                   for row in family
+                   for element in row), "List must contain ints or floats"
+    except AssertionError as bad_args:
+        print(bad_args)
+        return
+    newarray = nompy.array(family)
+    print("My shape is :", newarray.shape)
+    bim = slice(start, end)
+    newarray = newarray[bim]
+    print("My new shape is :", newarray.shape)
+    return newarray.tolist()
